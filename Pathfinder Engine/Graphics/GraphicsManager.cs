55a0d7899fbcf5a62a9d342e90449efee895eb33
Jonathan Sutton <comp8nerd4u2@gmail.com>
2016-07-20 03:07:10 -0500

    Changed Graphics to GraphicsManager.

--- Pathfinder Engine/Graphics/GraphicsManager.cs
+++ Pathfinder Engine/Graphics/GraphicsManager.cs
@@ -1,14 +1,16 @@
-﻿using System;
+﻿using SharpDX.Direct2D1;
+using ID2D1Factory = SharpDX.Direct2D1.Factory1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pathfinder_Engine.Graphics {
-    class Graphics {
+    class GraphicsManager {
 
         public static void Initialize() {
-
+            ID2D1Factory f = new ID2D1Factory(FactoryType.SingleThreaded, DebugLevel.None);
         }
 
         public static void test() {
